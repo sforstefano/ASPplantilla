3fc81852fa7eff96ba84ba0e41819fd1d3e16273
DAW2 <stefanocattoni98@gmail.com>
2019-04-09 18:54:33 +0200

    Restaurant acabat

--- PracticaASP/PracticaASP/Default.aspx.cs
+++ PracticaASP/PracticaASP/Default.aspx.cs
@@ -12,43 +12,20 @@ public partial class _Default : System.Web.UI.Page
 
     }
 
-    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-    {
-
-        int valor;
-        String missatge = "";
-
-        if (Int32.TryParse(importe.Text, out valor))
-        {
-
-            
-        }
-        else
-        {
-            missatge = "Sembla que no has introduït un import correcte...";
-        }
-
-        resul.Text = missatge;
-
-    }
 
     private String CalculImport(object sender, EventArgs e, decimal valor)
     {
         String dia;
         String mensaje = "";
-        dia = DropDownList1.DataValueField;
-        System.Diagnostics.Debug.WriteLine("Estamos dentro de calculo de importe");
+        dia = ((DropDownList)sender).SelectedValue;
 
-        System.Diagnostics.Debug.WriteLine("El valor de dia es " + dia);
         switch (dia)
         {
             case "1":
                 mensaje = diaSemana(valor, 10);
-                System.Diagnostics.Debug.WriteLine("Es del tipo de dia 1");
                 break;
             case "2":
                 mensaje = diaSemana(valor, 14);
-                System.Diagnostics.Debug.WriteLine("Es del tipo de dia 2");
                 break;
         }
 
@@ -62,7 +39,6 @@ public partial class _Default : System.Web.UI.Page
         String mensaje = "";
         
         importe = valor - preu;
-        System.Diagnostics.Debug.WriteLine("El importe a pagar es " + importe);
 
         if (importe > 0)
         {
@@ -89,11 +65,8 @@ public partial class _Default : System.Web.UI.Page
 
         pagado = decimal.Parse(importe.Text);
 
-        System.Diagnostics.Debug.WriteLine("El importe de pagado es " + pagado);
-
         if (!pagado.Equals(null))
         {
-            System.Diagnostics.Debug.WriteLine("Estamos dentro de que no és null");
             missatge = CalculImport(sender, e, pagado);
         }
 
