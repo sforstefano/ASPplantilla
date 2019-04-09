4fcbec84f8efdb05677194a8bc937cc5317ee99c
DAW2 <stefanocattoni98@gmail.com>
2019-04-09 19:59:05 +0200

    Numero perfecte acabat

--- PracticaASP/PracticaASP/Default2.aspx
+++ PracticaASP/PracticaASP/Default2.aspx
@@ -1,6 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-    Ivannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+    
+    <form id="form1" runat="server">
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="És perfecte?" OnClick="Button1_Click" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </form>
+    
 </asp:Content>
 
--- PracticaASP/PracticaASP/Default2.aspx.cs
+++ PracticaASP/PracticaASP/Default2.aspx.cs
@@ -11,4 +11,55 @@ public partial class Default2 : System.Web.UI.Page
     {
 
     }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine("Entra a la execusió del botó");
+
+        String value = TextBox1.Text;
+        int numero = Int32.Parse(value);
+        System.Diagnostics.Debug.WriteLine("El numero es " + numero);
+        String result = "";
+
+        result = es_perfecte(numero);
+        System.Diagnostics.Debug.WriteLine("La funcio retorna " + result);
+        Label1.Text = result;
+    }
+
+    private String es_perfecte(int numero)
+    {
+        String esPerf = "No és perfecte :(";
+        int limit;
+        int suma = 0;
+        int comodin = numero;
+
+        if (numero % 2 == 2)
+        {
+            limit = numero / 2;
+        }
+        else
+        {
+            numero += 1;
+            limit = numero / 2;
+        }
+
+        System.Diagnostics.Debug.WriteLine("El limit és " + limit);
+
+        for (int i = limit; i>0; i--)
+        {
+            if (comodin % i == 0)
+            {
+                suma += i;
+            }
+        }
+
+        System.Diagnostics.Debug.WriteLine("La suma és " + suma);
+
+        if (suma == comodin)
+        {
+            esPerf = "Sí és perfecte :)";
+        }
+
+        return esPerf;
+    }
 }
